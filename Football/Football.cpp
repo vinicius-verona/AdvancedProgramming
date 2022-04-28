e8f17509b180ef504bd11bc30a6f196579350769
Vinicius Verona <verona.projects@tutanota.com>
2022-04-27 21:56:19 -0300

    Add Footbal (AKA Soccer)

--- Football/Football.cpp
+++ Football/Football.cpp
@@ -1,17 +1,20 @@
 // Used references:
 // https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=1135
+// https://stackoverflow.com/questions/15927583/how-to-suppress-warning-control-reaches-end-of-non-void-function
+// https://github.com/Sharknevercries/Online-Judge/blob/master/UVA/Volume%20CI/10194%20Football%20(aka%20Soccer).cpp ->
+// Lexicographical sorting
 
 #include <algorithm>
 #include <climits>
+#include <cstring>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 using namespace std;
 
-#define TOURNAMENT_NAME 100
-#define TEAM_NAME       30
-#define GAME_MAXSIZE    2 * TEAM_NAME + 4
-
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreturn-type"
 typedef struct _team {
     string name;
     int teamRank;
@@ -25,7 +28,7 @@ typedef struct _team {
     int goalsDifference;
 
     /**
-     * @brief Compare two contestants according to the contest rules.
+     * @brief Compare two teams according to the tournament rules.
      * @param a Any team
      * @param b Any team
      * @note Comparison rule:
@@ -36,7 +39,7 @@ typedef struct _team {
      * 5. Less games played.
      * 6. Lexicographic order.
      */
-    bool operator<(const _team& team) const {
+    bool operator<(const _team &team) const {
         if (points != team.points)
             return points > team.points;
         else if (wins != team.wins)
@@ -47,10 +50,35 @@ typedef struct _team {
             return goalsScored > team.goalsScored;
         else if (games != team.games)
             return games < team.games;
-        else if (name != team.name)
-            return name < team.name;
+        else
+            // In order to sort in lexicographical order, consider:
+            // * each characters as lowercase
+            // * if string 'a' reaches its end, than it comes before 'b'
+            // * if string 'b' reaches its end, than it comes before 'a'
+            // * if the compared char from 'a' has a smaller ASCII value than char from 'b'. Then, 'a' comes before.
+            for (int i = 0; i >= 0; i++) {
+                if (name[i] == '\0') return true;
+                if (team.name[i] == '\0') return false;
+                if (tolower(name[i]) < tolower(team.name[i])) return true;
+                if (tolower(name[i]) > tolower(team.name[i])) return false;
+            }
     }
 } Team;
+#pragma GCC diagnostic pop
+
+// For a given pair of teams, compare both according to the tournament rules
+bool compareTeams(Team *a, Team *b) { return *a < *b; }
+
+// For a given team, calculates the amount of points earned
+int calculatePoints(Team team) { return 3 * team.wins + team.ties; }
+
+// Overloading operator for cout
+ostream &operator<<(ostream &os, const Team &team) {
+    os << team.teamRank << ") " << team.name << " " << calculatePoints(team) << "p, " << team.games << "g ("
+       << team.wins << "-" << team.ties << "-" << team.losses << "), " << team.goalsDifference << "gd ("
+       << team.goalsScored << "-" << team.goalsAgainst << ")";
+    return os;
+}
 
 /**
  * @brief Problem: 10194 - Football (aka Soccer)
@@ -66,38 +94,32 @@ int main() {
 
     for (int i = 0; i < tournaments; i++) {
         // Read tournament name
-        size_t buffersize = TOURNAMENT_NAME;
-        char* tournament = (char*)malloc((TOURNAMENT_NAME + 1) * sizeof(char));
-        getline(&tournament, &buffersize, stdin);
+        string tournament;
+        getline(cin, tournament);
 
         int nteams;
-        scanf("%d", &nteams);
-        map<string, Team*> teamsMap;
-        vector<Team*> teamsVector(nteams, Team());
+        scanf("%d\n", &nteams);
+        map<string, Team *> teamsMap;
+        vector<Team *> teamsVector(nteams, NULL);
 
         // Read and map teams
         for (int j = 0; j < nteams; j++) {
-            char* name = (char*)malloc((TEAM_NAME + 1) * sizeof(char));
-            buffersize = TEAM_NAME + 1;
-            getline(&name, &buffersize, stdin);
+            string name;
+            getline(cin, name);
 
-            int length = strlen(name);
-            name[length - 1] = '\0';
-
-            Team* team = new Team();
-            team->name teamsMap[name] = team;
+            Team *team = new Team();
+            team->name = name;
+            teamsMap[name] = team;
             teamsVector[j] = team;
-            free(name);
         }
 
         // Read and map each game to each team
         int ngames;
-        char* gameBuffer = (char*)malloc((GAME_MAXSIZE + 1) * sizeof(char));
-        buffersize = GAME_MAXSIZE + 1;
-        scanf("%d", &ngames);
+        string gameBuffer;
+        scanf("%d\n", &ngames);
 
         for (int j = 0; j < ngames; j++) {
-            int read = getline(&gameBuffer, &buffersize, stdin);
+            getline(cin, gameBuffer);
 
             string teamNameA, scoreA, teamNameB, scoreB;
             int goalsA, goalsB, idx = 0;
@@ -107,24 +129,57 @@ int main() {
             idx += 1;
             for (idx = idx; gameBuffer[idx] != '@'; idx++) scoreA.push_back(gameBuffer[idx]);
             idx += 1;
+
             goalsA = stoi(scoreA);
-            teamsMap[teamNameA].goalsScored = goalsA;
+            teamsMap[teamNameA]->goalsScored += goalsA;
 
             // Using map, map game to team B
-            for (idx = 0; gameBuffer[idx] != '#'; idx++) scoreB.push_back(gameBuffer[idx]);
+            for (idx = idx; gameBuffer[idx] != '#'; idx++) scoreB.push_back(gameBuffer[idx]);
             idx += 1;
             for (idx = idx; gameBuffer[idx] != '\n' && gameBuffer[idx] != '\0'; idx++)
                 teamNameB.push_back(gameBuffer[idx]);
             idx += 1;
-            goalsB = stoi(scoreB);
-            teamsMap[teamNameB].goalsScored = goalsB;
 
-            // Update goals against
-            teamsMap[teamNameA].goalsAgaint = goalsB;
-            teamsMap[teamNameB].goalsAgaint = goalsA;
+            goalsB = stoi(scoreB);
+            teamsMap[teamNameB]->goalsScored += goalsB;
+
+            // Update goals against, goals difference, and games
+            teamsMap[teamNameA]->goalsAgainst += goalsB;
+            teamsMap[teamNameB]->goalsAgainst += goalsA;
+            teamsMap[teamNameA]->goalsDifference += (goalsA - goalsB);
+            teamsMap[teamNameB]->goalsDifference += (goalsB - goalsA);
+            teamsMap[teamNameA]->games += 1;
+            teamsMap[teamNameB]->games += 1;
+
+            if (goalsA > goalsB) {
+                teamsMap[teamNameA]->wins += 1;
+                teamsMap[teamNameA]->points += 3;
+                teamsMap[teamNameB]->losses += 1;
+            } else if (goalsA < goalsB) {
+                teamsMap[teamNameB]->wins += 1;
+                teamsMap[teamNameB]->points += 3;
+                teamsMap[teamNameA]->losses += 1;
+            } else {
+                teamsMap[teamNameA]->ties += 1;
+                teamsMap[teamNameB]->ties += 1;
+                teamsMap[teamNameA]->points += 1;
+                teamsMap[teamNameB]->points += 1;
+            }
         }
 
-        // sort(submissions.begin(), submissions.end());
+        sort(teamsVector.begin(), teamsVector.end(), compareTeams);
+
+        //  Print the result
+        cout << tournament << endl;
+        int counter = 1;
+        for (Team *team : teamsVector) {
+            team->teamRank = counter;
+            cout << *team << endl;
+            counter++;
+        }
+
+        teamsMap.clear();
+        teamsVector.clear();
 
         if (i < tournaments - 1) printf("\n");
     }
